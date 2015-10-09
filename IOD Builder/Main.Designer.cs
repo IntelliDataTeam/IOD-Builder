f31e08c167aee730d402b37e5227ae843ba1b95a
Quang Nguyen <quang.nguyen@intellidata.net>
2015-10-09 15:49:54 -0600

    Added the framework for load_data

---
+++ IOD Builder/Main.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace IOD_Builder
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.filePath = new System.Windows.Forms.TextBox();
+            this.browseBtn = new System.Windows.Forms.Button();
+            this.buildBtn = new System.Windows.Forms.Button();
+            this.cancelBtn = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.percentOut = new System.Windows.Forms.Label();
+            this.outputBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // filePath
+            // 
+            this.filePath.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.filePath.Location = new System.Drawing.Point(12, 25);
+            this.filePath.Name = "filePath";
+            this.filePath.ReadOnly = true;
+            this.filePath.Size = new System.Drawing.Size(286, 20);
+            this.filePath.TabIndex = 0;
+            // 
+            // browseBtn
+            // 
+            this.browseBtn.Location = new System.Drawing.Point(304, 23);
+            this.browseBtn.Name = "browseBtn";
+            this.browseBtn.Size = new System.Drawing.Size(75, 23);
+            this.browseBtn.TabIndex = 1;
+            this.browseBtn.Text = "Browse...";
+            this.browseBtn.UseVisualStyleBackColor = true;
+            this.browseBtn.Click += new System.EventHandler(this.browseBtn_Click);
+            // 
+            // buildBtn
+            // 
+            this.buildBtn.Location = new System.Drawing.Point(211, 272);
+            this.buildBtn.Name = "buildBtn";
+            this.buildBtn.Size = new System.Drawing.Size(75, 23);
+            this.buildBtn.TabIndex = 2;
+            this.buildBtn.Text = "Build";
+            this.buildBtn.UseVisualStyleBackColor = true;
+            this.buildBtn.Click += new System.EventHandler(this.buildBtn_Click);
+            // 
+            // cancelBtn
+            // 
+            this.cancelBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelBtn.Location = new System.Drawing.Point(304, 272);
+            this.cancelBtn.Name = "cancelBtn";
+            this.cancelBtn.Size = new System.Drawing.Size(75, 23);
+            this.cancelBtn.TabIndex = 3;
+            this.cancelBtn.Text = "Cancel";
+            this.cancelBtn.UseVisualStyleBackColor = true;
+            this.cancelBtn.Click += new System.EventHandler(this.cancelBtn_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 66);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(340, 32);
+            this.progressBar.TabIndex = 4;
+            // 
+            // percentOut
+            // 
+            this.percentOut.AutoSize = true;
+            this.percentOut.Location = new System.Drawing.Point(358, 76);
+            this.percentOut.Name = "percentOut";
+            this.percentOut.Size = new System.Drawing.Size(21, 13);
+            this.percentOut.TabIndex = 5;
+            this.percentOut.Text = "0%";
+            // 
+            // outputBox
+            // 
+            this.outputBox.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.outputBox.Location = new System.Drawing.Point(12, 115);
+            this.outputBox.Multiline = true;
+            this.outputBox.Name = "outputBox";
+            this.outputBox.ReadOnly = true;
+            this.outputBox.Size = new System.Drawing.Size(367, 121);
+            this.outputBox.TabIndex = 6;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 307);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.percentOut);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.cancelBtn);
+            this.Controls.Add(this.buildBtn);
+            this.Controls.Add(this.browseBtn);
+            this.Controls.Add(this.filePath);
+            this.Name = "Main";
+            this.Text = "IOD Builder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox filePath;
+        private System.Windows.Forms.Button browseBtn;
+        private System.Windows.Forms.Button buildBtn;
+        private System.Windows.Forms.Button cancelBtn;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label percentOut;
+        private System.Windows.Forms.TextBox outputBox;
+    }
+}
+
---
+++ IOD Builder/Main.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Configuration;
+using Excel;
+
+namespace IOD_Builder
+{
+    public partial class Main : Form
+    {
+        #region Global Variables
+        private BackgroundWorker minion = new BackgroundWorker();
+        List<List<string>> pkgList;
+        #endregion
+
+        public Main()
+        {
+            InitializeComponent();
+            minion.DoWork += new DoWorkEventHandler(minion_DoWork);
+            minion.ProgressChanged += new ProgressChangedEventHandler(minion_ProgressChanged);
+            minion.RunWorkerCompleted += new RunWorkerCompletedEventHandler(minion_RunWorkerCompleted);
+        }
+
+        #region backgroundworker
+        void minion_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            foreach (List<string> l in pkgList)
+            {
+                Console.WriteLine(string.Join(",",l.ToArray()));
+            }
+            cancelBtn.Text = "Cancel";
+        }
+
+        void minion_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+           
+        }
+
+        void minion_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Load_Data();
+        }
+        #endregion
+
+        #region GUI Controls
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            minion.WorkerReportsProgress = true;
+            minion.WorkerSupportsCancellation = true;
+        }
+
+        private void browseBtn_Click(object sender, EventArgs e)
+        {
+            string fn;
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Select XLSX File";
+            ofd.InitialDirectory = "c:\\Documents";
+            ofd.Filter = "CSV files (*.xlsx)|*.xlsx|All files (*.*)|*.*";
+            ofd.FilterIndex = 1;
+            ofd.RestoreDirectory = true;
+            DialogResult result = ofd.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                fn = ofd.FileName;
+                filePath.Text = fn;
+            }
+            else if (result == DialogResult.Cancel)
+            {
+
+            }
+        }
+
+        private void buildBtn_Click(object sender, EventArgs e)
+        {
+            if (filePath.Text == "")
+            {
+                outputBox.Text = "Please select an .xlsx file first.";
+            }
+            else
+            {
+                outputBox.Text = "Building...";
+                cancelBtn.Text = "Stop";
+                minion.RunWorkerAsync();
+            }
+        }
+
+        private void cancelBtn_Click(object sender, EventArgs e)
+        {
+            if (minion.IsBusy)
+            {
+                minion.CancelAsync();
+                cancelBtn.Text = "Cancel";
+            }
+            else
+                Environment.Exit(0);
+        }
+        #endregion
+
+        #region Internal Functions
+        private void Load_Data()
+        {
+            int count = 0;
+            pkgList = new List<List<string>>();
+            //Need a config file that will identify the worksheet for the program.
+            foreach (worksheet ws in Workbook.Worksheets(filePath.Text))
+            {
+                switch(count) {
+                    case 0:
+                        SetPkgSpecs(ws);
+                    break;
+
+                    case 1:
+                        List<List<string>> c0g = new List<List<string>>();
+                        SetSeriesInfo(ws,c0g);
+                    break;
+
+                    default:
+                    break;
+                }
+                count++;
+            }
+        }
+
+        private void SetPkgSpecs(worksheet ws)
+        {
+            pkgList = new List<List<string>>();
+            int r = 1;
+
+            while (ws.Rows[r].Cells[3] != null)
+            {
+                List<string> rowData = new List<string>();
+                for (int c = 3; c < 9; c++)
+                {
+                    rowData.Add(ws.Rows[r].Cells[c].Text);
+                }
+                pkgList.Add(rowData);
+                r++;
+            }
+        }
+
+        private void SetSeriesInfo(worksheet ws, List<List<string>> series)
+        {
+            
+        }
+        #endregion
+    }
+}
